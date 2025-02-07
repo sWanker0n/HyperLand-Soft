d38ae6ab9035f10bac7339242d0650c68c115b3e
Danyla <danyla.kpk@gmail.com>
2025-02-08 00:09:49 +0300

    working state of the software

--- config.py
+++ config.py
@@ -1,9 +1,44 @@
+import json
 
+with open('data/abi/faucet_abi.json', 'r') as file:
+    FAUCET_ABI = json.load(file)
+
+with open('data/abi/erc_20_abi.json', 'r') as file:
+    ERC_20_ABI = json.load(file)
+
+with open('data/abi/hyperLend_pool.json', 'r') as file:
+    HYPERLEND_POOL_ABI = json.load(file)
+
+headers = {
+                'authority': 'api.hyperlend.finance',
+                'method': 'POST',
+                "path": "",
+                "scheme": "https",
+                'Accept': 'application/json',
+                'Accept-Language': 'en-US',
+                'connection': 'keep-alive',
+                'Origin': 'https://testnet.hyperlend.finance',
+                'Referer': 'https://testnet.hyperlend.finance/',
+                'User-Agent': "",
+            }
 
 # --------------------------------- FAUCET -----------------------------------------------
 
-two_captcha_api_key = ''
-SLEEP_MIN = 1
-SLEEP_MAX = 10
+two_captcha_api_key = 'YOUT_2CAPTCHA_API_KEY'
+SLEEP_MIN = 1  # faucet sleep min
+SLEEP_MAX = 10 # faucet sleep max
 
 # --------------------------------- TESTNET -----------------------------------------------
+
+rpc = 'https://testnet.hyperlend.finance'
+
+TESTNET_SLEEP_MIN = 1 # testnet sleep min
+TESTNET_SLEEP_MAX = 60 # testnet sleep max
+
+contracts = {
+    'token_mockBTC': '0x453b63484b11bbF0b61fC7E854f8DAC7bdE7d458',
+    'mockBTC_faucet': '0x941559af458a9a0448b411a26047584b506283a7',
+    'HyperLend_pool': '0x1e85CCDf0D098a9f55b82F3E35013Eda235C8BD8',
+    'HyperLend: Variable Debt MBTC': '0x742d75d7389E66f6C17898A0e19077E17F1C51d1',
+    'HyperLend: Lend hMBTC': '0xde72990638db12f8AA4cd9406bA6c648153A5cEA'
+}
--- hyper_lend.py
+++ hyper_lend.py
@@ -1,12 +1,20 @@
+import random
+
 from loguru import logger as ll
 import requests
 import time
 from data_manager import DataManager
 import fake_useragent
-from config import two_captcha_api_key
-class HyperLand:
-    def __init__(self, address):
-        self.address = address
+from config import two_captcha_api_key, FAUCET_ABI, contracts, headers, HYPERLEND_POOL_ABI
+from account import Account
+from eth_account.messages import encode_defunct
+from faker import Faker
+
+
+
+class HyperLend(Account):
+    def __init__(self, pk):
+        super().__init__(pk=pk)
         self.url = "https://testnet.hyperlend.finance/dashboard"
         self.captcha_token = '0x4AAAAAAA2Qg1SB87LOUhrG'
         self.CAPTCHA_API_KEY = two_captcha_api_key
@@ -14,6 +22,10 @@ class HyperLand:
         self.session = requests.session()
         self.Data = DataManager()
         self.proxy = self.Data.get_proxy()
+
+        self.faucet_contract = self.get_contract(contract_address=contracts['mockBTC_faucet'], abi=FAUCET_ABI)
+        self.hyperLend_pool_contract = self.get_contract(contract_address=contracts["HyperLend_pool"], abi=HYPERLEND_POOL_ABI)
+
 
     def check_proxy(self):
         # FORMAT http://{proxy_username}:{proxy_password}@{http_proxy_url}
@@ -95,24 +107,41 @@ class HyperLand:
         ll.error(f"Cant solve captcha")
         return False
 
+    def request_mockBTC(self):
+        if self.w3.is_connected():
+            if self.check_balance() >= 50000000000000:
+                try:
+                    isClaimed = self.faucet_contract.functions.isClaimed(add=self.w3.to_checksum_address(self.address)).call()
+                    if isClaimed:
+                        ll.warning(f"[{self.address}] already claimed mockBTC")
+                        return False
+                    ll.info(f"[{self.address}] trying to claim mockBTC...")
+                    tx_data = self.get_tx_data()
+                    transaction = self.faucet_contract.functions.claim().build_transaction(tx_data)
+                    signed_txn = self.sign(transaction)
+                    txn_hash = f"0x{self.send_raw_transaction(signed_txn).hex()}"
+                    ll.success(f"[{self.address}] https://testnet.purrsec.com/tx/{txn_hash}")
+                except Exception as err:
+                    ll.error(err)
+                    return False
+
+            else:
+                ll.warning(f"[{self.address}] don't have enough HYPE balance ")
+                return False
+
+        else:
+            ll.error("Cant connect to RPC")
+            return False
+
     def faucet(self):
         self.get_account_session()
+        ll.info(f"[{self.address}] trying to claim HYPER...")
         token = self.reCaptchaV2()
         if token == False:
             return False
         url = 'https://api.hyperlend.finance/ethFaucet'
-        headers = {
-            'authority': 'api.hyperlend.finance',
-            'method': 'POST',
-            "path": "/ethFaucet",
-            "scheme": "https",
-            'Accept': 'application/json',
-            'Accept-Language': 'en-US',
-            'connection': 'keep-alive',
-            'Origin': 'https://testnet.hyperlend.finance',
-            'Referer': 'https://testnet.hyperlend.finance/',
-            'User-Agent': self.UA,
-        }
+        headers['path'] = "/ethFaucet"
+        headers['User-Agent'] = self.UA
         body = {
             "challenge": token,
             "challengeV2": "If you are running the farming bot, stop wasting your time. Testnet will not be directly incentivized, and mainnet airdrop will be linear with a minimum threshold.",
@@ -123,13 +152,114 @@ class HyperLand:
             response = self.session.post(url=url, json=body, headers=headers)
             if response.status_code == 200:
                 if response.json().get('response') == 'user_already_claimed':
-                    ll.warning(f"Address [{self.address}] {response.json().get('response')}")
+                    ll.warning(f"[{self.address}] {response.json().get('response')} HYPER")
+                    self.request_mockBTC()
                 elif response.json().get('response').get('status') == 1:
-                    ll.success(f"Address [{self.address}] tokens received")
+                    ll.success(f"[{self.address}] tokens received")
+                    sleep = random.randint(1, 20)
+                    ll.info(f'Sleep {sleep} sec')
+                    time.sleep(sleep)
+                    self.request_mockBTC()
             else:
                 ll.error(f"Status code {response.status_code}")
         except Exception as err:
             ll.error(err)
             return False
+
+    def check_username(self):
+        try:
+            url = f'https://api.hyperlend.finance/leaderboard/name/get?address={self.address}'
+            headers['path'] = f"/leaderboard/name/get?address={self.address}"
+            headers['User-Agent'] = self.UA
+            response = self.session.get(url=url, headers=headers)
+
+            if response.json().get('name') == None:
+                return True
+            else:
+                return False
+
+        except Exception as err:
+            ll.error(err)
+            return False
+
+    def set_username(self):
+        fake = Faker()
+        username = fake.user_name()
+        status = self.check_username()
+
+        if status == False:
+            ll.warning(f"[{self.address}] username already registered")
+            return False
+
+        try:
+
+            ll.info(f"[{self.address}] try to register username: {username}")
+            text = f'hyperlend_leaderborad_name_{self.address.lower()}_{username}'
+            message = encode_defunct(text=text)
+            signed_message = self.w3.eth.account.sign_message(message, private_key=self.pk)
+            signature = f"0x{signed_message.signature.hex()}"
+            url = f'https://api.hyperlend.finance/leaderboard/name/set?address={self.address}&signature={signature}&name={username}'
+            headers['path'] = f"leaderboard/name/set?address={self.address}&signature={signature}&name={username}"
+            response = self.session.get(url=url, headers=headers)
+
+            if response.json().get('msg') == 'name updated':
+                ll.success(f'[{self.address}] successfully registered username: {username}')
+            else:
+                ll.warning(f'[{self.address}] status code {response.status_code}')
+
+        except Exception as err:
+            ll.error(err)
+
+    def lend(self):
+        try:
+            if self.check_balance() >= 500000000000:
+                btc_balance = self.get_balance(contract_address=contracts["token_mockBTC"])
+                if btc_balance >= 1000000:
+                    lend_amount = int(btc_balance * (random.randint(850, 950) / 1000))
+                    ll.info(f"[{self.address}] try to lend {lend_amount / 10 ** 8 } WBTC")
+                    self.approve(amount=self.w3.to_wei(random.randint(1, 1000) / 10, "ether"), token_address=contracts['token_mockBTC'], contract_address=contracts['HyperLend_pool'])
+                    tx_data = self.get_tx_data()
+                    transaction = self.hyperLend_pool_contract.functions.supply(token_address=self.w3.to_checksum_address(contracts['token_mockBTC']), amount=lend_amount, wallet_address=self.w3.to_checksum_address(self.address), something=0).build_transaction(tx_data)
+                    signed_txn = self.sign(transaction)
+                    txn_hash = f"0x{self.send_raw_transaction(signed_txn).hex()}"
+                    ll.success(f"[{self.address}] lended https://testnet.purrsec.com/tx/{txn_hash}")
+                    return True
+                else:
+                    ll.warning(f"[{self.address}] dont have enough mockBTC for lend")
+                    return False
+            else:
+                ll.warning(f"[{self.address}] dont have enough HYPE balance to pay fees")
+                return False
+        except Exception as err:
+            ll.error(err)
+            return False
+
+    def borrow(self):
+        try:
+            if self.check_balance() >= 500000000000:
+                lend_btc_balance = self.get_balance(contract_address=contracts["HyperLend: Lend hMBTC"])
+                borrowed_btc_balance = self.get_balance(contract_address=contracts["HyperLend: Variable Debt MBTC"])
+                LVT = int(lend_btc_balance * 0.74) - borrowed_btc_balance
+                if LVT >= 700000:
+                    borrow_amount = int(LVT * (random.randint(900, 990)/1000))
+                    ll.info(f"[{self.address}] try to borrow {borrow_amount / 10 ** 8 } WBTC")
+                    tx_data = self.get_tx_data()
+                    transaction = self.hyperLend_pool_contract.functions.borrow(token_address=self.w3.to_checksum_address(contracts['token_mockBTC']), amount=borrow_amount, something1=2, something2=0, wallet_address=self.w3.to_checksum_address(self.address)).build_transaction(tx_data)
+                    signed_txn = self.sign(transaction)
+                    txn_hash = f"0x{self.send_raw_transaction(signed_txn).hex()}"
+                    ll.success(f"[{self.address}] borrowed https://testnet.purrsec.com/tx/{txn_hash}")
+                    return True
+                else:
+                    ll.warning(f"[{self.address}] dont have enough LTV for borrowing, you need to lend more WBTC")
+                    return False
+            else:
+                ll.warning(f"[{self.address}] dont have enough HYPE balance to pay fees")
+                return False
+        except Exception as err:
+            ll.error(err)
+            return False
+
+
+
 
 
